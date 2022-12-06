7bbfa77c0fdf1031b46a4dd5dc49a12808a11b11
Chris Hanson <chanson8@gmail.com>
2022-12-06 14:24:37 -0600

    able to select governor

--- scripts/Facilities.js
+++ scripts/Facilities.js
@@ -1,14 +1,21 @@
 import { getFacilities } from "./database.js";
+import { getcurrentGovernorId } from "./database.js";
 
 const facilities = getFacilities();
 
+
 export const Facilities = () => {
-  let html = "<label for='facility-select'>Choose a facility</label>";
-  html +=
-    "<select name='facilities' id='facility-select'><option disabled selected value='0'>Choose a facility...</option>";
-  for (const facility of facilities) {
-    html += `<option value="${facility.id}">${facility.name}</option>`;
+  const governorId = getcurrentGovernorId();
+  let html;
+  if (governorId === 0) {
+    html = "<h4>Please choose a governor before choosing a facility</h4>";
+  } else {
+    html =
+      "<select name='facilities' id='facility-select'><option disabled selected value='0'>Choose a facility...</option>";
+    for (const facility of facilities) {
+      html += `<option value="${facility.id}">${facility.name}</option>`;
+    }
+    html += "</select>";
   }
-  html += "</select>";
   return html;
 };
--- scripts/Governors.js
+++ scripts/Governors.js
@@ -1,14 +1,20 @@
-import { getGovernors } from "./database.js";
+import { getGovernors, setCurrentGovernorId } from "./database.js";
 
-const governors = getGovernors();
+
 
 export const Governors = () => {
-  let html = "<label for='governor-select'>Choose a governor</label>";
-  html +=
-    "<select name='governors' id='governor-select'><option disabled selected value='0'>Choose a Governor...</option>";
+  const governors = getGovernors();
+  let html = "<select name='governors' id='governor-select'><option disabled selected value='0'>Choose a Governor...</option>";
   for (const governor of governors) {
     html += `<option value="${governor.id}">${governor.name}</option>`;
   }
   html += "</select>";
   return html;
 };
+
+document.addEventListener("change", (event) => {
+  if (event.target.id === 'governor-select') {
+    let clickedGovernorId = event.target.value;
+    setCurrentGovernorId(clickedGovernorId);
+  }
+})
